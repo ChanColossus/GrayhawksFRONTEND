47224e7871dd102c320efe5e2a133607d3f711c3
ChanColossus <christianbenedict.quintero@tup.edu.ph>
2024-03-17 10:26:27 +0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { Container, TextField, Button, Typography, Select, MenuItem, FormControl, InputLabel,Table, TableHead, TableRow, TableCell, TableBody  } from '@mui/material';
+import { Grid,Container, TextField, Button, Typography, Select, MenuItem, FormControl, InputLabel,Table, TableHead, TableRow, TableCell, TableBody  } from '@mui/material';
 import axios from 'axios';
 import { Navbar, NavbarBrand } from "reactstrap"; // Changed import statement
+import { makeStyles } from '@mui/styles';
 import Logo from "./grx.png"
-
+import Day1 from "./Day1.jpg"
+import Day2 from "./Day2.jpg"
+import Day3 from "./Day3.jpg"
+import Footer from "./Footer.png"
+import { AppBar, Toolbar } from '@mui/material';
+import Carousel from 'react-elastic-carousel';
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    zIndex: 1000,
+  },
+}));
 function App() {
+  const classes = useStyles();
   const [formData, setFormData] = useState({
     fname: '',
     lname: '',
@@ -16,6 +32,8 @@ function App() {
   });
 
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(10);
   const [usersCount, setUsersCount] = useState(0);
   const [createErrors, setCreateErrors] = useState({}); 
   const handleChange = (e) => {
@@ -87,27 +105,48 @@ function App() {
       // Handle error (e.g., display error message to the user)
     }
   };
-
+  const carouselItems = [
+    { id: 1, image: Day1 },
+    { id: 2, image: Day2 },
+    { id: 3, image: Day3 }
+  ];
   useEffect(() => {
     fetchUsers();
   }, []);
+ // Logic to calculate index of the last entry on the current page
+ const indexOfLastUser = currentPage * usersPerPage;
+ // Logic to calculate index of the first entry on the current page
+ const indexOfFirstUser = indexOfLastUser - usersPerPage;
+ // Slice the users array to display only entries for the current page
+ const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
 
+ // Change page
+ const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <>
-      <Navbar style={{ backgroundImage: "linear-gradient(to right, darkred, red)", color: "#fff", padding: "0 20px", paddingTop: "15px" }}>
-        <Container>
-          <NavbarBrand style={{ display: "flex", alignItems: "center" }}>
-            <img src={Logo} alt="Logo" style={{ width: "70px", marginRight: "20px" }} />
-            <Typography variant="h6">Grayhawks Esports</Typography>
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-     
-     
-      <Container maxWidth="lg" style={{ marginTop: '20px', display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '20px' }}>
+    <style>
+      {`
+        body {
+          background: linear-gradient(to top, rgba(255,0,0,0.5), rgba(255,255,255,0.5));
+          font-family: 'Michroma', sans-serif;
+        }
+      `}
+    </style>
+    <AppBar position="fixed">
+      <Toolbar style={{ backgroundImage: "linear-gradient(to right, darkred, red)", color: "#fff", padding: "0 20px", paddingTop: "10px" }}>
+        <img src={Logo} alt="Logo" style={{ width: "70px", marginRight: "20px" }} />
+        <Typography variant="h6" style={{ fontFamily: 'Michroma, sans-serif', flexGrow: 1 }}>Grayhawks Esports</Typography>
+        <Typography variant="h6" style={{ fontFamily: 'Michroma, sans-serif' }}>Esportsfest 2024</Typography>
+      </Toolbar>
+    </AppBar>
+   
+
+<Grid container style={{ height: '100vh', marginTop: '70px',paddingLeft:"20px" }} spacing={3}>
+
+<Grid item xs={3}>
         
-        <div>
-          <Typography variant="h5" component="h2" gutterBottom>Registration Form</Typography>
+        <div >
+          <Typography variant="h5" component="h2" gutterBottom style={{ textAlign: 'center',fontFamily: 'Michroma, sans-serif'  }}>Registration Form</Typography>
           <form >
             <TextField
               label="First Name"
@@ -118,6 +157,9 @@ function App() {
               fullWidth
               margin="normal"
               required
+              InputProps={{
+                style: { border: '2px solid maroon' },
+              }}
             />
             <Typography> {createErrors.fname && <span className="text-danger" style={{color:"red"}}>{createErrors.fname}</span>}</Typography>
             <TextField
@@ -129,18 +171,25 @@ function App() {
               fullWidth
               margin="normal"
               required
+              InputProps={{
+                style: { border: '2px solid maroon' },
+              }}
             />
             <Typography> {createErrors.lname && <span className="text-danger" style={{color:"red"}}>{createErrors.lname}</span>}</Typography>
             <TextField
-              label="Email"
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-              required
-            />
+  label="Email"
+  type="email"
+  name="email"
+  value={formData.email}
+  onChange={handleChange}
+  fullWidth
+  margin="normal"
+  required
+  InputProps={{
+    style: { border: '2px solid maroon' },
+  }}
+/>
+
             <Typography> {createErrors.email && <span className="text-danger" style={{color:"red"}}>{createErrors.email}</span>}</Typography>
             <TextField
               label="Contact Number"
@@ -151,6 +200,9 @@ function App() {
               fullWidth
               margin="normal"
               required
+              InputProps={{
+                style: { border: '2px solid maroon' },
+              }}
             />
             <Typography> {createErrors.contact && <span className="text-danger" style={{color:"red"}}>{createErrors.contact}</span>}</Typography>
              <TextField
@@ -161,8 +213,11 @@ function App() {
                 onChange={handleChange}
                 fullWidth
                 margin="normal"
+                InputProps={{
+                  style: { border: '2px solid maroon' },
+                }}
               />
-              <FormControl fullWidth margin="normal">
+              <FormControl fullWidth margin="normal" style={{ border: '2px solid maroon', borderRadius: '3px' }}> 
                 <InputLabel id="cbzndc-label">CBZN Discord</InputLabel>
                 <Select
                   labelId="cbzndc-label"
@@ -171,68 +226,112 @@ function App() {
                   value={formData.cbzndc}
                   onChange={handleChange}
                   required
+                  InputProps={{
+                    style: { border: '2px solid maroon' },
+                  }}
                 >
                   <MenuItem value="True">True</MenuItem>
                   <MenuItem value="False">False</MenuItem>
                 </Select>
               </FormControl>
-              <FormControl fullWidth margin="normal">
-                <InputLabel id="givelabs-label">Givelabs Requirements</InputLabel>
-                <Select
-                  labelId="givelabs-label"
-                  id="givelabs"
-                  name="givelabs"
-                  value={formData.givelabs}
-                  onChange={handleChange}
-                  required
-                >
-                  <MenuItem value="True">True</MenuItem>
-                  <MenuItem value="False">False</MenuItem>
-                </Select>
-              </FormControl>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              fullWidth
-              style={{ marginTop: '1rem' }}
-              onClick={handleSubmit}
-            >
-              Register
-            </Button>
+              <FormControl fullWidth margin="normal" style={{ border: '2px solid maroon', borderRadius: '3px' }}>
+  <InputLabel id="givelabs-label">Givelabs Requirements</InputLabel>
+  <Select
+    labelId="givelabs-label"
+    id="givelabs"
+    name="givelabs"
+    value={formData.givelabs}
+    onChange={handleChange}
+    required
+  >
+    <MenuItem value="True">True</MenuItem>
+    <MenuItem value="False">False</MenuItem>
+  </Select>
+</FormControl>
+
+              <Button
+  type="submit"
+  variant="contained"
+  fullWidth
+  sx={{
+    marginTop: '1rem',
+    backgroundColor: 'maroon',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: 'rgba(0, 0, 0, 0.2)', // Low opacity black
+    },
+  }}
+  onClick={handleSubmit}
+>
+  Register
+</Button>
           </form>
         </div>
+        </Grid>
+
+<Grid item xs={5}>
         <div>
  
-          <Typography variant="h5" component="h2" gutterBottom>Total Entries:{usersCount}</Typography>
-          <Table>
+          <Typography variant="h5" component="h2" gutterBottom style={{fontFamily: 'Michroma, sans-serif' }}>Total Entries:{usersCount}</Typography>
+          <Table style={{ border: '4px solid maroon' }}>
             <TableHead>
               <TableRow>
-                <TableCell>First Name</TableCell>
-                <TableCell>Last Name</TableCell>
-                <TableCell>Email</TableCell>
-                <TableCell>Contact Number</TableCell>
-                <TableCell>Discord ID</TableCell>
-                <TableCell>CBZN Discord</TableCell>
-                <TableCell>Givelabs Requirements</TableCell>
+                <TableCell  style={{ borderBottom: '2px solid maroon' }}>First Name</TableCell>
+                <TableCell style={{ borderBottom: '2px solid maroon' }}>Last Name</TableCell>
+                <TableCell style={{ borderBottom: '2px solid maroon' }}>Email</TableCell>
+                <TableCell style={{ borderBottom: '2px solid maroon' }}>Contact Number</TableCell>
+                <TableCell style={{ borderBottom: '2px solid maroon' }}>Discord ID</TableCell>
+                <TableCell style={{ borderBottom: '2px solid maroon' }}>CBZN Discord</TableCell>
+                <TableCell style={{ borderBottom: '2px solid maroon' }}>Givelabs Requirements</TableCell>
               </TableRow>
             </TableHead>
-            <TableBody>
-              {users.map((user, index) => (
-                <TableRow key={index}>
-                  <TableCell>{user.fname}</TableCell>
-                  <TableCell>{user.lname}</TableCell>
-                  <TableCell>{user.email}</TableCell>
-                  <TableCell>{user.contact}</TableCell>
-                  <TableCell>{user.dcId}</TableCell>
-                  <TableCell>{user.cbzndc}</TableCell>
-                  <TableCell>{user.givelabs}</TableCell>
+            <TableBody >
+              {currentUsers.map((user, index) => (
+                <TableRow key={index} >
+                  <TableCell style={{ borderBottom: '2px solid maroon' }}>{user.fname}</TableCell>
+                  <TableCell style={{ borderBottom: '2px solid maroon' }}>{user.lname}</TableCell>
+                  <TableCell style={{ borderBottom: '2px solid maroon' }}>{user.email}</TableCell>
+                  <TableCell style={{ borderBottom: '2px solid maroon' }}>{user.contact}</TableCell>
+                  <TableCell style={{ borderBottom: '2px solid maroon' }}>{user.dcId}</TableCell>
+                  <TableCell style={{ borderBottom: '2px solid maroon' }}>{user.cbzndc}</TableCell>
+                  <TableCell style={{ borderBottom: '2px solid maroon' }}>{user.givelabs}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
+          <br/>
+          <div style={{ textAlign: 'center' }}>
+  {Array.from({ length: Math.ceil(users.length / usersPerPage) }, (_, i) => (
+    <Button
+      key={i}
+      onClick={() => paginate(i + 1)}
+      variant={currentPage === i + 1 ? 'contained' : 'outlined'}
+      style={{ backgroundColor: 'maroon', color: 'white' }}
+    >
+      {i + 1}
+    </Button>
+  ))}
+</div>
         </div>
-      </Container>
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+  <div style={{ height: '100%', width: '100%',paddingTop:"50px" }}>
+  <Typography variant="h2" component="h2" gutterBottom style={{ textAlign: 'center', fontFamily: 'Michroma, sans-serif'  }}>
+  Schedule
+</Typography>
+    <Carousel style={{ height: '100%', width: '100%' }}>
+      {carouselItems.map(item => (
+        <img key={item.id} src={item.image} alt={`Day ${item.id}`} style={{ objectFit: 'cover', width: '100%', height: '100%',border: '4px solid maroon' }} />
+      ))}
+    </Carousel>
+  </div>
+</Grid>
+
+</Grid>
+<div className={classes.footer}>
+      <img src={Footer} alt="Footer" style={{ width: '100%', height: 'auto' }} />
+    </div>
     </>
   );
 }
